b3c3c0ed1d0740ef8d3998a294528b8d80bbd897
SJTUPanda <37395676+SJTUPanda@users.noreply.github.com>
2018-09-08 20:24:32 +0800

    Update 小题目记录.py

--- 小题目记录.py
+++ 小题目记录.py
@@ -20,3 +20,22 @@ while(True):
             temp=temp.replace(s[i],"")
             ans=ans+s[i]
     print(temp+ans)
+
+#已知前序和中序，求二叉树的后序排列
+def res(pre,tin,ans):
+    if pre==[] or tin==[]:
+        return ans
+    else:
+        cur=tin.index(pre[0])
+        pre1=pre[1:cur+1]
+        pre2=pre[cur+1:]
+        tin1=tin[:cur]
+        tin2=tin[cur+1:]
+    ans=res(pre1,tin1,ans) 
+    ans=res(pre2,tin2,ans)
+    ans.append(pre[0])   
+    return ans
+ans=[]
+pre=[1,2,4,7,3,5,6,8]
+tin=[4,7,2,1,5,3,8,6]
+print(res(pre,tin,ans))
